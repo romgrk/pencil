384cccf7f7af33ddc4ecc2d27d9c187c23f211fd
Rom Grk <romgrk.cc@gmail.com>
2024-04-06 00:21:34 -0400

    update

--- src/demo/Pencil.ts
+++ src/demo/Pencil.ts
@@ -1,4 +1,4 @@
-import { svg, Matrix } from '2d-geometry'
+import { svg, Matrix, Box } from '2d-geometry'
 import { Graph } from '../graph/Graph'
 import { Container } from '../graph/Container'
 import { Node } from '../graph/Node'
@@ -17,24 +17,40 @@ const colors = [
 ]
 
 const paths = svg.parsePath(pencilPath)
+const boundingBox = paths.reduce((b, path) => b.merge(path.box), Box.EMPTY)
 
 export class Pencil extends Graph {
   constructor(domNode: any, options: any) {
     super(domNode, options)
 
     this.root.add(new Container([new elements.Grid()]))
-    const content = new Container([], Matrix.IDENTITY.translate(100, 89))
-    this.root.add(content)
+    const container = new Container([], Matrix.IDENTITY.translate(100, 89))
+    const content = new Container([], Matrix.IDENTITY.translate(boundingBox.center.x, boundingBox.center.y))
+    container.add(content)
+    this.root.add(container)
 
-    animate({ duration: 3000 }, f => {
-      content.clear()
-      paths.forEach((path, i) => {
-        content.add(
-          new Node(path.slice(0, path.length * f), Style.from({ lineWidth: 3, strokeStyle: colors[i % colors.length] }))
-        )
+
+    let animations = []
+    for (let i = 0; i < paths.length; i++) {
+      const path = paths[i]
+      const node = new Node(path.slice(0, path.length * 0), Style.from({ lineWidth: 1, strokeStyle: colors[i % colors.length] }))
+      node.x = -boundingBox.center.x
+      node.y = -boundingBox.center.y
+      content.add(node)
+
+      const a = animate({ delay: i * 400, duration: 2000 }, f => {
+        node.shape = path.slice(0, path.length * f)
+        this.render()
       })
-      this.render()
-    })
+      animations.push(a)
+    }
+
+    Promise.all(animations).then(() =>
+      animate({ duration: 250 }, f => {
+        content.alpha = 1 - f
+        this.render()
+      })
+    )
 
     this.render()
   }
