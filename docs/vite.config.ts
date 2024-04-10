19b486a979d649ac6191c787dc44d6f8114d9b0b
Rom Grk <romgrk.cc@gmail.com>
2024-04-10 16:07:32 -0400

    chore: build sourcemaps

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -4,6 +4,9 @@ import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
   plugins: [react()],
   resolve: {
     alias: {
